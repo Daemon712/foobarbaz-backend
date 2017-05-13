0ed2e59ecc8ced816844c7c49a9df10e2ee679c4
Dmitriy Baboshkin <Daemon712@yandex.ru>
2017-05-13 15:00:24 +0400

    fix rating when user details is not created

--- src/main/java/ru/foobarbaz/logic/impl/RatingServiceImpl.java
+++ src/main/java/ru/foobarbaz/logic/impl/RatingServiceImpl.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 import ru.foobarbaz.entity.challenge.Challenge;
+import ru.foobarbaz.entity.challenge.personal.ChallengeUserDetails;
 import ru.foobarbaz.entity.challenge.personal.ChallengeUserRating;
 import ru.foobarbaz.entity.user.UserAccount;
 import ru.foobarbaz.entity.challenge.personal.ChallengeUserPK;
 import ru.foobarbaz.logic.RatingService;
 import ru.foobarbaz.repo.ChallengeRepository;
 import ru.foobarbaz.repo.UserAccountRepository;
+import ru.foobarbaz.repo.UserChallengeDetailsRepository;
 import ru.foobarbaz.repo.UserChallengeRatingRepository;
 
 import javax.transaction.Transactional;
@@ -17,25 +19,30 @@ import javax.transaction.Transactional;
 @Service
 public class RatingServiceImpl implements RatingService {
     private ChallengeRepository challengeRepository;
-    private UserChallengeRatingRepository userChallengeRatingRepository;
+    private UserChallengeRatingRepository userRatingRepository;
+    private UserChallengeDetailsRepository userDetailsRepository;
     private UserAccountRepository userAccountRepository;
 
     @Autowired
     public RatingServiceImpl(
             ChallengeRepository challengeRepository,
-            UserChallengeRatingRepository userChallengeRatingRepository,
+            UserChallengeRatingRepository userRatingRepository,
+            UserChallengeDetailsRepository userDetailsRepository,
             UserAccountRepository userAccountRepository) {
         this.challengeRepository = challengeRepository;
-        this.userChallengeRatingRepository = userChallengeRatingRepository;
+        this.userRatingRepository = userRatingRepository;
+        this.userDetailsRepository = userDetailsRepository;
         this.userAccountRepository = userAccountRepository;
     }
 
     @Override
     @Transactional
-    public ChallengeUserRating updateChallengeRating(ChallengeUserRating rating) {
-        ChallengeUserPK pk = rating.getPk();
-        ChallengeUserRating oldRating = userChallengeRatingRepository.findById(pk).orElse(null);
-        ChallengeUserRating newRating = userChallengeRatingRepository.save(rating);
+    public ChallengeUserRating updateChallengeRating(ChallengeUserRating newRating) {
+        ChallengeUserPK pk = newRating.getPk();
+        ChallengeUserDetails userDetails = userDetailsRepository.findById(pk).orElse(new ChallengeUserDetails(pk));
+        ChallengeUserRating oldRating = userDetails.getRating();
+        userDetails.setRating(newRating);
+        userDetailsRepository.save(userDetails);
 
         Challenge challenge = challengeRepository.findById(pk.getChallengeId())
                 .orElseThrow(ResourceNotFoundException::new);
@@ -43,7 +50,7 @@ public class RatingServiceImpl implements RatingService {
         if (!pk.getUsername().equals(challenge.getAuthor().getUsername()))
             updateUserAccountRating(challenge.getAuthor().getUsername(), oldRating, newRating);
 
-        ChallengeUserRating avgRating = userChallengeRatingRepository.calcAvgRating(challenge.getChallengeId());
+        ChallengeUserRating avgRating = userRatingRepository.calcAvgRating(challenge.getChallengeId());
         challenge.setRating(avgRating.getRating());
         challenge.setDifficulty(avgRating.getDifficulty());
         challengeRepository.save(challenge);
