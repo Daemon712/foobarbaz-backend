9984271698297ffc852bf800cf09b56094f1889b
Dmitriy <Daemon712@yandex.ru>
2017-05-19 06:43:37 +0400

    make app able to deploy on tomcat

--- src/main/java/ru/foobarbaz/FoobarbazApplication.java
+++ src/main/java/ru/foobarbaz/FoobarbazApplication.java
@@ -5,14 +5,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @SpringBootApplication
-public class FoobarbazApplication {
+public class FoobarbazApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoobarbazApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FoobarbazApplication.class);
 	}
 
 	@Bean
